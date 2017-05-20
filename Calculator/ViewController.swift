ca760a101eb027e34b581af2faf7c861e36955e8
eric yu <ericyu423@erics-MacBook-Pro.local>
2017-05-20 15:18:29 -0700

    break

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        let textCurrentlyInDisplay = display.text!
+        display.text = textCurrentlyInDisplay + digit
+        
+        print("touch\(digit) digit")
+   
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
